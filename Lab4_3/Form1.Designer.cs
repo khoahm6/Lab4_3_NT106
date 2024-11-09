9eea14c0624dc3e55b4295d26fbcf6d226b208e2
congtuAnHuu <khoah0605@gmail.com>
2024-11-09 19:16:47 +0700

    Lab4 bai3

---
+++ Lab4_3/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Lab4_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.addressBar = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.downfilesButton = new System.Windows.Forms.Button();
+            this.downRButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(-1, 64);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(801, 384);
+            this.webView.Source = new System.Uri("https://uit.edu.vn/", System.UriKind.Absolute);
+            this.webView.TabIndex = 1;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // addressBar
+            // 
+            this.addressBar.Location = new System.Drawing.Point(93, 12);
+            this.addressBar.Name = "addressBar";
+            this.addressBar.Size = new System.Drawing.Size(584, 22);
+            this.addressBar.TabIndex = 2;
+            this.addressBar.Text = "https://uit.edu.vn/";
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(12, 11);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 3;
+            this.goButton.Text = "Load";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(684, 11);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(90, 23);
+            this.btnReload.TabIndex = 4;
+            this.btnReload.Text = "reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // downfilesButton
+            // 
+            this.downfilesButton.Location = new System.Drawing.Point(515, 40);
+            this.downfilesButton.Name = "downfilesButton";
+            this.downfilesButton.Size = new System.Drawing.Size(103, 23);
+            this.downfilesButton.TabIndex = 5;
+            this.downfilesButton.Text = "Down Files";
+            this.downfilesButton.UseVisualStyleBackColor = true;
+            this.downfilesButton.Click += new System.EventHandler(this.downfilesButton_Click);
+            // 
+            // downRButton
+            // 
+            this.downRButton.Location = new System.Drawing.Point(638, 40);
+            this.downRButton.Name = "downRButton";
+            this.downRButton.Size = new System.Drawing.Size(136, 23);
+            this.downRButton.TabIndex = 6;
+            this.downRButton.Text = "Down Resources ";
+            this.downRButton.UseVisualStyleBackColor = true;
+            this.downRButton.Click += new System.EventHandler(this.downRButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.downRButton);
+            this.Controls.Add(this.downfilesButton);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.addressBar);
+            this.Controls.Add(this.webView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.TextBox addressBar;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.Button downfilesButton;
+        private System.Windows.Forms.Button downRButton;
+    }
+}
+
---
+++ Lab4_3/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using System.Net;
+using System.IO;
+using System.Security.Policy;
+using HtmlAgilityPack;
+namespace Lab4_3
+{
+    public partial class Form1 : Form
+    {
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            webView.NavigationStarting += EnsureHttps;
+            InitializeAsync();
+        }
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.WebMessageReceived += UpdateAddressBar;
+
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.addEventListener(\'message\', event => alert(event.data));");
+        }
+
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            addressBar.Text = uri;
+            webView.CoreWebView2.PostWebMessageAsString(uri);
+        }
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView.Size = this.ClientSize - new System.Drawing.Size(webView.Location);
+            goButton.Left = this.ClientSize.Width - goButton.Width;
+            addressBar.Width = goButton.Left - addressBar.Left;
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(addressBar.Text);
+            }
+        }
+        void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            String uri = args.Uri;
+            if (!uri.StartsWith("https://"))
+            {
+                webView.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
+                args.Cancel = true;
+            }
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            webView.CoreWebView2?.Reload();
+        }
+
+        private void downfilesButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient myClient = new WebClient();
+                string url = addressBar.Text; // Lấy URL từ thanh địa chỉ
+                string filePath = "D:\\MyWeb\\MyDownLoadHTML.html"; // Đường dẫn lưu tệp
+
+                // Tải xuống và lưu tệp HTML
+                myClient.DownloadFile(url, filePath);
+
+                MessageBox.Show("Tải xuống thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải xuống: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+private void downRButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string url = addressBar.Text;
+            string saveDirectory = "D:\\MyWeb";
+
+            // Tạo thư mục nếu chưa tồn tại
+            if (!Directory.Exists(saveDirectory))
+            {
+                Directory.CreateDirectory(saveDirectory);
+            }
+
+            WebClient webClient = new WebClient();
+            // Tải mã HTML của trang
+            string htmlContent = webClient.DownloadString(url);
+
+                // Phân tích mã HTML
+             HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+             document.LoadHtml(htmlContent);
+
+            // Trích xuất tất cả các liên kết đến hình ảnh, CSS, và JS
+            var imageNodes = document.DocumentNode.SelectNodes("//img[@src]");
+            var cssNodes = document.DocumentNode.SelectNodes("//link[@rel='stylesheet']");
+            var scriptNodes = document.DocumentNode.SelectNodes("//script[@src]");
+
+            // Tải hình ảnh
+            if (imageNodes != null)
+            {
+                foreach (var node in imageNodes)
+                {
+                    string imageUrl = node.GetAttributeValue("src", "");
+                    DownloadResource(imageUrl, saveDirectory, webClient);
+                }
+            }
+
+            // Tải CSS
+            if (cssNodes != null)
+            {
+                foreach (var node in cssNodes)
+                {
+                    string cssUrl = node.GetAttributeValue("href", "");
+                    DownloadResource(cssUrl, saveDirectory, webClient);
+                }
+            }
+
+            // Tải JavaScript
+            if (scriptNodes != null)
+            {
+                foreach (var node in scriptNodes)
+                {
+                    string scriptUrl = node.GetAttributeValue("src", "");
+                    DownloadResource(scriptUrl, saveDirectory, webClient);
+                }
+            }
+
+            MessageBox.Show("Tải xuống tài nguyên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Lỗi khi tải xuống: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    // Hàm hỗ trợ để tải và lưu tài nguyên
+    private void DownloadResource(string resourceUrl, string saveDirectory, WebClient webClient)
+    {
+        if (!Uri.IsWellFormedUriString(resourceUrl, UriKind.Absolute))
+        {
+            // Xử lý các URL tương đối
+            resourceUrl = new Uri(new Uri(addressBar.Text), resourceUrl).ToString();
+        }
+
+        string fileName = Path.GetFileName(new Uri(resourceUrl).AbsolutePath);
+        string savePath = Path.Combine(saveDirectory, fileName);
+
+        webClient.DownloadFile(resourceUrl, savePath);
+    }
+
+}
+}
